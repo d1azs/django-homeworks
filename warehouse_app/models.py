f80f7bc1bd3642e5d39882b3f4cfc1617b6201eb
Tohqa <tohqa@MacBook-Air-Tohqa.local>
2025-11-24 10:38:15 +0200

    Define Product model for stationery (HW 2.5)

--- warehouse_app/models.py
+++ warehouse_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    item_name = models.CharField(max_length=200, verbose_name="Item name")
+    brand = models.CharField(max_length=100, verbose_name="Brand")
+    category = models.CharField(max_length=100, verbose_name="Category")
+    quantity_in_pack = models.IntegerField(verbose_name="Quantity in pack")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Price")
+
+    def __str__(self):
+        return f"{self.item_name} ({self.brand}) - {self.quantity_in_pack} pcs"
