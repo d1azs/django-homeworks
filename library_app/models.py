d9354c5a762ff2a0b65466e51c9ff0b961cece03
Tohqa <tohqa@MacBook-Air-Tohqa.local>
2025-11-24 10:25:45 +0200

    Define Author and Book models

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="First name")
+    last_name = models.CharField(max_length=100, verbose_name="Last name")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    publishing_year = models.IntegerField(verbose_name="Publishing year")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Price")
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="books",
+        verbose_name="Author",
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publishing_year})"
